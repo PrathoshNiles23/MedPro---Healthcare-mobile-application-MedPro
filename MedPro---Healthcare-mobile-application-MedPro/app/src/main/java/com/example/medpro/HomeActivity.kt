6b96f9ef0c0aebbe67a197b1e3f45e7f8a735508
Prathosh <it21347726@my.sliit.lk>
2023-06-23 11:14:12 +0530

    first commit

---
+++ MedPro---Healthcare-mobile-application-MedPro/app/src/main/java/com/example/medpro/HomeActivity.kt
@@ -0,0 +1,91 @@
+package com.example.medpro
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+
+class HomeActivity : AppCompatActivity() {
+    private lateinit var profile: CardView
+    private lateinit var article: CardView
+    private lateinit var logout: Button
+    private lateinit var medi: CardView
+    private lateinit var doc:CardView
+    private lateinit var lab:CardView
+    private lateinit var appointments: CardView
+    private lateinit var mylabs: CardView
+    private lateinit var cart: CardView
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        profile = (findViewById(R.id.cardMyProfile))
+        logout = findViewById(R.id.lout)
+        article = findViewById(R.id.cardArticles)
+        medi = findViewById(R.id.cardMedicine)
+        doc = findViewById(R.id.cardDoctor)
+        lab = findViewById(R.id.cardLabTest)
+        appointments = findViewById(R.id.cardAppointment)
+        mylabs = findViewById(R.id.cardMyLabs)
+        cart = findViewById(R.id.cardCart)
+
+
+
+        val sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE)
+        val email = sharedPreferences.getString("email", "").toString()
+        Toast.makeText(applicationContext, "Welcome ${email}!!!", Toast.LENGTH_LONG).show()
+
+        profile.setOnClickListener {
+            val intent = Intent(this, UserProfileActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        logout.setOnClickListener {
+            val editor = sharedPreferences.edit()
+            editor.clear()
+            editor.apply()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        article.setOnClickListener {
+            startActivity(Intent(this, ArticleActivity::class.java))
+        }
+
+        medi.setOnClickListener {
+            startActivity(Intent(this, MedicineActivity::class.java))
+        }
+
+        doc.setOnClickListener{
+            startActivity(Intent(this, DoctorActivity::class.java))
+        }
+
+        lab.setOnClickListener{
+            startActivity(Intent(this, LabActivity::class.java))
+
+        }
+
+        appointments.setOnClickListener{
+            startActivity(Intent(this, AllAppointments::class.java))
+        }
+
+        mylabs.setOnClickListener{
+            startActivity(Intent(this, MyLabs::class.java))
+        }
+
+        cart.setOnClickListener{
+            startActivity(Intent(this, Cart::class.java))
+        }
+
+
+    }
+
+
+}
---
+++ MedPro---Healthcare-mobile-application-MedPro/app/src/main/java/com/example/medpro/UpdateActivity.kt
@@ -0,0 +1,66 @@
+package com.example.medpro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class UpdateActivity : AppCompatActivity() {
+    lateinit var home: Button
+    lateinit var email: TextView
+    lateinit var name: EditText
+    lateinit var phone: EditText
+    lateinit var password: EditText
+    lateinit var update: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+        home = findViewById(R.id.btnHome)
+        email = findViewById(R.id.Email)
+        name = findViewById(R.id.Up_Name)
+        phone = findViewById(R.id.Up_Phone)
+        password = findViewById(R.id.Up_Password)
+        update = findViewById(R.id.btnUpdate)
+        var db = com.example.medpro.Controllers.UserC(applicationContext, "Medpro", null, 1)
+
+        val sharedPreferences = getSharedPreferences("shared_prefs", MODE_PRIVATE)
+        val e = sharedPreferences.getString("email", "").toString()
+        val x = db.getId(e)
+
+        var a: com.example.medpro.Models.User = db.profile(e)
+
+        name.setText(a.fullname)
+        phone.setText(a.phone)
+        password.setText(a.password)
+        email.text = e
+
+
+        update.setOnClickListener {
+
+            a.fullname = name.text.toString()
+            a.phone = phone.text.toString()
+            a.password = password.text.toString()
+            if (db.updateProfile(a)) {
+                Toast.makeText(
+                    applicationContext,
+                    "Your account updated successfully",
+                    Toast.LENGTH_LONG
+                ).show()
+                startActivity(Intent(this, UserProfileActivity::class.java))
+            } else {
+                Toast.makeText(applicationContext, "Could't update your account", Toast.LENGTH_LONG)
+                    .show()
+
+            }
+
+        }
+
+        home.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+
+        }
+    }
+}
